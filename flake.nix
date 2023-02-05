5e5e5b517d06ddb2945ef4be43fd919dbc67255c
John Boehr <john@johnboehr.is>
2023-02-04 16:26:27 -0800

    [zig/nix] add derivation to build zig

--- flake.nix
+++ flake.nix
@@ -3,11 +3,26 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
     flake-utils.url = github:numtide/flake-utils;
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zig-overlay.url = "github:mitchellh/zig-overlay/17352071583eda4be43fa2a312f6e061326374f7";
+    zig-sdl = {
+      url = "github:MasterQ32/SDL.zig/6a9e37687a4b9ae3c14c9ea148ec51d14e01c7db";
+      flake = false;
+    };
+    zig-clap = {
+      url = "github:Hejsil/zig-clap/e5d09c4b2d121025ad7195b2de704451e6306807";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+  outputs = { self, nixpkgs, flake-utils, gitignore, zig-overlay, zig-sdl, zig-clap, ... }@inputs: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = nixpkgs.legacyPackages.${system};
     lib = pkgs.lib;
+    inherit (gitignore.lib) gitignoreSource;
+    zig = zig-overlay.packages.${system}.master-2022-11-29;
 
 
     # Get each directory with a `shell.nix`:
@@ -31,10 +46,20 @@
         utilsShell
       ];
     });
-  in {
+    
+    mkZig = {safeSupport ? false, fastSupport ? false}: pkgs.callPackage ./zig/derivation.nix {
+      inherit zig zig-sdl zig-clap safeSupport fastSupport gitignoreSource;
+    };
+  in rec {
+    packages = {
+      zig = mkZig { fastSupport = true; };
+      zig-safe = mkZig { safeSupport = true; };
+    };
+
     devShells = langDevShells // {
       default = pkgs.mkShell { inputsFrom = builtins.attrValues langDevShells; };
       utils = utilsShell;
+      zig = pkgs.mkShell { inputsFrom = [ packages.zig ]; };
     };
   });
 }
