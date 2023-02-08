a494fb3dd706dc524ad4230657f944f4b267811a
Rahul Butani <rrbutani@users.noreply.github.com>
2023-02-08 12:50:10 -0600

    [py/nix] manually override `mypy` to 1.0.0 for now

--- py/derivation.nix
+++ py/derivation.nix
@@ -2,21 +2,39 @@
   lib,
   python311,
   gitignoreSource,
+  fetchFromGitHub,
   mypycSupport ? false
 }:
 
 let
   python = python311;
   pythonPackages = python.pkgs;
+
+  # We use `match` which is only supported in `mypy` v1 and newer:
+  # https://github.com/python/mypy/commit/d5e96e381f72ad3fafaae8707b688b3da320587d
+  #
+  # This hasn't made it's way into `nixpkgs` yet so we override for now:
+  # (!!! remove once nixpkgs has 1.0.0+)
+  mypy' = pythonPackages.mypy.overridePythonAttrs (old: rec {
+    version = "1.0.0";
+    buildInputs = (old.buildInputs or []) ++ (with pythonPackages; [ psutil types-psutil ]);
+    src = fetchFromGitHub {
+      owner = "python";
+      repo = "mypy";
+      rev = "refs/tags/v${version}";
+      hash = "sha256-/E2O6J+o0OiY2v/ogatygaB07D/Z5ZQ6mB0daEQqo+4=";
+    };
+  });
+
   runtimeDeps = with pythonPackages; [ setuptools pysdl2 ];
-  devDeps = with pythonPackages; [ mypy black ];
+  devDeps = with pythonPackages; [ mypy' black ];
 in
 
 pythonPackages.buildPythonApplication rec {
   name = "rosettaboy-py";
   src = gitignoreSource ./.;
 
-  nativeBuildInputs = lib.optional mypycSupport pythonPackages.mypy;
+  nativeBuildInputs = lib.optional mypycSupport mypy';
 
   passthru.python = python.withPackages (p: runtimeDeps ++ devDeps);
   passthru.devTools = [ python ];
