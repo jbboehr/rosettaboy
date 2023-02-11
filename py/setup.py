8bb5a65c0753127582275c7ce78deb14733e3ead
John Boehr <john@johnboehr.is>
2023-02-11 14:51:01 -0800

    [py/nix] Undo the local imports and package mapping to fix mypyc

--- py/setup.py
+++ py/setup.py
@@ -5,25 +5,23 @@ USE_MYPYC = False
 if os.getenv('ROSETTABOY_USE_MYPYC', None) == '1':
     USE_MYPYC = True
 
-ext_modules = []
 if USE_MYPYC:
     from mypyc.build import mypycify
-    ext_modules=mypycify([
-        'src',
-    ])
+    packages = []
+    ext_modules=mypycify(
+        ['src']
+    )
+else:
+    packages=['src']
+    ext_modules = []
 
 setup(
     name='rosettaboy',
     description='A sample Python package',
-    packages=['rosettaboy'],
-    package_dir={'rosettaboy': 'src'},
-    install_requires=[
-        'pysdl2',
-    ],
-    py_modules=['rosettaboy.main'],
+    packages=packages,
     entry_points={
         'console_scripts': [
-            'rosettaboy-py=rosettaboy:main.cli_main',
+            'rosettaboy-py=src:main.cli_main',
         ],
     },
     ext_modules=ext_modules,
